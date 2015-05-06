1aa03f967dc2f75d2f5e25d62943b764a4ddfbf0
Vladislav Yarovyi <skygo67@gmail.com>
2015-05-07 01:48:46 +0300

    first commit

---
+++ desktop/src/com/skynet67/pixeldungeon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.skynet67.pixeldungeon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.skynet67.pixeldungeon.PixelDungeon;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PixelDungeon(), config);
+	}
+}
