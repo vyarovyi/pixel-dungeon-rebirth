a0c32e339d6282223ce7f2e21c453b4906feef15
Vladislav Yarovyi <skygo67@gmail.com>
2015-05-24 21:40:25 +0300

    Android immersive mode fix

--- android/src/com/bravepixel/pixeldungeon/android/AndroidLauncher.java
+++ android/src/com/bravepixel/pixeldungeon/android/AndroidLauncher.java
@@ -4,45 +4,86 @@ import android.annotation.SuppressLint;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.bravepixel.pixeldungeon.android.AndroidInputProcessor;
 import com.watabou.input.NoosaInputProcessor;
 import com.watabou.pixeldungeon.PixelDungeon;
-import com.watabou.pixeldungeon.input.GameAction;
 import com.watabou.utils.PDPlatformSupport;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		String version;
-		try {
-			version = getPackageManager().getPackageInfo( getPackageName(), 0 ).versionName;
-		} catch (PackageManager.NameNotFoundException e) {
-			version = "???";
-		}
-		initialize(new PixelDungeon(new AndroidPlatformSupport(version, "BravePixel/PixelDungeon", new AndroidInputProcessor())), config);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	private class AndroidPlatformSupport extends PDPlatformSupport {
-		public AndroidPlatformSupport(String version, String basePath, NoosaInputProcessor inputProcessor) {
-			super(version, basePath, inputProcessor);
-		}
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        String version;
+        try {
+            version = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            version = "???";
+        }
+        initialize(new PixelDungeon(new AndroidPlatformSupport(version, "BravePixel/PixelDungeon", new AndroidInputProcessor())), config);
+    }
 
-		@SuppressLint("NewApi")
-		@Override
-		public boolean isImmersiveModeEnabled() {
-			return android.os.Build.VERSION.SDK_INT >= 19;
-		}
+    @Override
+    public void onWindowFocusChanged( boolean hasFocus ) {
 
-		@Override
-		public void onOrientationChanged(boolean value) {
-			setRequestedOrientation(value ?
-					ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE :
-					ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		}
-	}
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus) {
+            try{
+                PixelDungeon.instance.getPlatformSupport().updateImmersiveMode();
+            } catch (Exception e){
+                PixelDungeon.reportException(e);
+            }
+        }
+    }
+
+    private class AndroidPlatformSupport extends PDPlatformSupport {
+        public AndroidPlatformSupport(String version, String basePath, NoosaInputProcessor inputProcessor) {
+            super(version, basePath, inputProcessor);
+        }
+
+        @Override
+        public boolean isImmersiveModeEnabled() {
+            return android.os.Build.VERSION.SDK_INT >= 19;
+        }
+
+        @Override
+        public void onOrientationChanged(boolean value) {
+            setRequestedOrientation(value ?
+                    ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE :
+                    ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+
+        @SuppressLint("NewApi")
+        @Override
+        public void updateImmersiveMode() {
+            if (isImmersiveModeEnabled()) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+
+                            // Sometime NullPointerException happens here
+                            getWindow().getDecorView().setSystemUiVisibility(
+                                    PixelDungeon.immersed() ?
+                                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                                                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                                                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                                                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                                                    View.SYSTEM_UI_FLAG_FULLSCREEN |
+                                                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                                            :
+                                            0);
+                        } catch (Exception e) {
+                            PixelDungeon.reportException(e);
+                        }
+                    }
+                });
+            }
+        }
+    }
 }
